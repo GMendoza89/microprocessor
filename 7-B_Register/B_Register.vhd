6d42d1d48001e5d73b6bc15275de710a913084a5
Gustavo Mendoza <gustavo.mendoza.dp@gmail.com>
2019-03-21 12:17:37 -0600

    Registro para manejo y control relacionado a bits

--- 7-B_Register/B_Register.vhd
+++ 7-B_Register/B_Register.vhd
@@ -13,66 +13,44 @@ Entity B_Register is
 End B_Register;
 
 Architecture Behavioral of B_Register is
-signal Qp,Qn,Qb : std_logic_vector(n-1 downto 0);
-Signal HB,HBC   : std_logic;
+signal Qp,Qn : std_logic_vector(n-1 downto 0);
+Signal HBP,HBN   : std_logic;
 Begin
     
-    
-    BitCombinational: process(DIN,SelB)
-    begin
-        case SelB is
-            when "000" =>
-                Qb <= DIN(n-1 downto 1)&HB;
-                HBC <= DIN(0);
-             when "001" =>
-                Qb <= Din(n-1 downto 2)&HB&DIN(0);
-                HBC <= DIN(1);
-             when "010" =>
-                Qb <= Din(n-1 downto 3)&HB&DIN(1 downto 0);
-                HBC <= DIN(2);
-             when "011" =>
-                Qb <= Din(n-1 downto 4)&HB&DIN(2 downto 0);
-                HBC <= DIN(3);
-            when "100" =>
-                Qb <= Din(n-1 downto 5)&HB&DIN(3 downto 0);
-                HBC <= DIN(4);
-            when "101" =>
-                Qb <= Din(n-1 downto 6)&HB&DIN(4 downto 0);
-                HBC <= DIN(5);
-            when "110" =>
-                Qb <= Din(n-1)&HB&DIN(5 downto 0);
-                HBC <= DIN(6);
-            when others =>
-                Qb <= HB&DIN(6 downto 0);
-                HBC <= DIN(7);
-    end case;
-     end process BitCombinational;
+    Dout <= Qp;
+    DBout <= HBP;
 
-
-	Combinational: process (Qp,OPC,DIN)
+	Combinational: process (Qp,OPC,DIN,SelB)
 	begin
 		case OPC is
-            when "00" => HB <='0';                 -- Data bit clear
-                         Qn <= Qb;
-                         DBout <= 'X';
-            when "01" => HB <= '1';
-                         QN <= Qb;                -- Data Bit set
-                         DBout <= 'X';
-            when "10" => HB <= NOT(DIN(to_integer(unsigned(SelB)))); -- Data Bit toogle
-                         Qn <= Qb;
-                         DBout <= 'X';
-            when others => Qn <= Din;              -- Data Bit Read
-                           HB <= 'X';
-                           DBout <= HBC;
+            when "00" => HBN <='0';                 -- Data bit clear
+                         Qn <= DIN;
+            when "01" => HBN <= '1';
+                         QN <= DIN;                -- Data Bit set
+            when "10" => HBN <= NOT(DIN(to_integer(unsigned(SelB)))); -- Data Bit toogle
+                         Qn <= DIN;
+            when others => Qn <= DIN;              -- Data Bit Read
+                           HBN <= DIN(to_integer(unsigned(SelB)));
 		end case;
 	end process Combinational;
-	Dout <= Qp;
-	Secuential: process(RST,CLK)
+
+	Secuential: process(RST,CLK,SelB)
 	begin
 		if(RST='0') then
 			Qp <= (others => '0');
+            HBP <='0';
 		elsif(CLK'event and CLK='1') then
-			Qp <= Qn;
+			HBP <= HBN;
+            case SelB is
+                when "000" => Qp <= Qn(n-1 downto 1)&HBN;
+                when "001" => Qp <= Qn(n-1 downto 2)&HBN&Qn(0);
+                when "010" => Qp <= Qn(n-1 downto 3)&HBN&Qn(1 downto 0);
+                when "011" => Qp <= Qn(n-1 downto 4)&HBN&Qn(2 downto 0);
+                when "100" => Qp <= Qn(n-1 downto 5)&HBN&Qn(3 downto 0);
+                when "101" => Qp <= Qn(n-1 downto 6)&HBN&Qn(4 downto 0);
+                when "110" => Qp <= Qn(n-1)&HBN&Qn(5 downto 0);
+                when others => Qp <=HBN&Qn(6 downto 0);
+            end case;
 		end if;
 	end Process Secuential;
 End Behavioral;
