e42f38995f7ef82c26be452f1217431382748190
GMendoza89 <gustavo.mendoza.dp@gmail.com>
2019-03-28 16:58:10 -0600

    Miltiplexor de 8 bits que conecta la salida de ram o de la memoria de programa con la entrada A de la ALU

---
+++ 11-Mux_8bit/Mux_8bit.vhd
@@ -0,0 +1,22 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.all;
+
+Entity Mux_8bit is
+    Port(
+    AI : in  std_logic_vector(7 downto 0);
+    AR : in  std_logic_vector(7 downto 0);
+    SM8: in  std_logic;
+    A  : out std_logic_vector(7 downto 0));
+End Mux_8bit;
+
+Architecture Behavioral of Mux_8bit is
+Begin
+    process(SM8)
+    begin
+        if(SM8 = '1') then
+            A <= AR;
+        else
+            A <= AI;
+        end if;
+    end Process;
+End Behavioral;
