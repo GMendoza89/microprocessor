5ddd9f0894705e6d59ce85a2004d6b07691a752e
GMendoza89 <gustavo.mendoza.dp@gmail.com>
2019-03-30 00:36:24 -0600

    Actualizaciones de señales

--- 08-Control_Unit/Decode.vhd
+++ 08-Control_Unit/Decode.vhd
@@ -3,16 +3,15 @@ USE IEEE.std_logic_1164.all;
 
 Entity Decode is
     Port(
-    CLK  : in  std_logic;
-    RST  : in  std_logic;
-    M    : in  std_logic_vector(16 downto 0);
-    Enc  : in  std_logic;
-    BCMD : out std_logic;
-    Ki   : out std_logic_vector(8 downto 0);
-    Ai   : out std_logic_vector(7 downto 0);
-    SD   : out std_logic_vector(7 downto 0);
-    SelB : out std_logic_vector(2 downto 0);
-    IST  : out std_logic_vector(5 downto 0));
+    CLK    : in  std_logic;                       -- Relog Maestro
+    RST    : in  std_logic;                       -- Reset Maestro
+    M      : in  std_logic_vector(16 downto 0);   -- Entrada de Trama de la ROM
+    Enc    : in  std_logic;                       -- Habilitacion de Decodificacion
+    SDM8   : out std_logic;                       -- Seleccionador entre guardar en W o En memoria RAM DEMUX 8 Bits
+    DataKi : out std_logic_vector(8 downto 0);    -- Direccion de Contador de Programa
+    Data   : out std_logic_vector(7 downto 0);    -- Direccion de Memoria o literal
+    SelB   : out std_logic_vector(2 downto 0);    -- Seleccionador de Bit
+    IST    : out std_logic_vector(5 downto 0));   -- Instruccion
 End Decode;
 
 Architecture Decoding of Decode is
@@ -36,11 +35,10 @@ Begin
         end if;
     end Process Sequential;
 
-    IST <= Qp(16 downto 11);
-    Ai <= Qp(10 downto 3);
-    SD <= Qp(10 downto 3);
-    Ki <= Qp(10 downto 2);
-    BCMD <= Qp(3);
-    SelB <= Qp(2 downto 0);
+    IST   <= Qp(16 downto 11);
+    Data  <= Qp(10 downto 3);
+    DataKi<= Qp(10 downto 2);
+    SDM8  <= NOT(Qp(2));
+    SelB  <= Qp(2 downto 0);
 End Decoding;
 
