6c6ac3e9debe355d0e1d8c2502b4463f4e0dfd2e
GMendoza89 <gustavo.mendoza.dp@gmail.com>
2019-04-08 17:13:29 -0500

    Multiplicador con registros secuenciales

--- 06-Multiplier/Multiplier.vhd
+++ 06-Multiplier/Multiplier.vhd
@@ -7,16 +7,33 @@ Entity Multiplier is
 			 double: integer:= 16);
    port
    (
-      Nibble1, Nibble2: in std_logic_vector(bn-1 downto 0);
+    RST, CLK, EnML : in std_logic;
+      A, W: in std_logic_vector(bn-1 downto 0);
 	  Rl,Rh:	out std_logic_vector(bn-1 downto 0)
    );
 End Multiplier;
 
 Architecture Behavioral of Multiplier is
-    signal Result : unsigned(double - 1 downto 0);
+    signal Qn,Qp : unsigned(double - 1 downto 0);
 Begin
+    Combinational: Process(EnML,A,W,Qp)
+    begin
+        if(EnML = '0') then 
+            Qn <= unsigned(A) * unsigned(W);
+        else
+            Qn <= Qp;
+        End if;
+    end Process Combinational;
 
-   Result <= unsigned(Nibble1) * unsigned(Nibble2);
-   rl <= std_logic_vector(Result(bn-1 downto 0));
-   rh <= std_logic_vector(Result(double-1 downto bn));
+    Sequential: Process(RST,CLK)
+    begin
+        if(RST = '0') then
+            Qp <= (others => '0');
+        elsif(CLK'event and CLK = '1') then
+            Qp <= Qn;
+        end if;
+    end Process Sequential;
+
+   rl <= std_logic_vector(Qp(bn-1 downto 0));
+   rh <= std_logic_vector(Qp(double-1 downto bn));
 End Behavioral;
