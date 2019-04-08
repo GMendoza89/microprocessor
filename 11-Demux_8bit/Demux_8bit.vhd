9a1c5d1fe5df0ca8d8a97b1aa7f6ec858d241481
GMendoza89 <gustavo.mendoza.dp@gmail.com>
2019-04-08 17:20:34 -0500

    Correccion de errores

--- 11-Demux_8bit/Demux_8bit.vhd
+++ 11-Demux_8bit/Demux_8bit.vhd
@@ -4,6 +4,7 @@ USE IEEE.std_logic_1164.all;
 Entity Demux_8bit is
     port(
     R   : in  std_logic_vector(7 downto 0);
+	EnDM8: in std_logic;
     SD8 : in  std_logic;
     AR  : out std_logic_vector(7 downto 0);
     BW  : out std_logic_Vector(7 downto 0));
@@ -11,17 +12,26 @@ Entity Demux_8bit is
 End Demux_8bit;
 
 Architecture Behavioral of Demux_8bit is
+signal Ww,WR : std_logic;
 Begin
-    Process(SD8)
+	--control de salidas a el BUS Bidireccional de la RAM y Registro W
+	Ww <= not(EnDM8) and SD8;
+	WR <= EnDM8 NOR SD8;
+    Process(R, Ww,WR)
     begin
-        if(SD8 = '1') then
+		
+        if(WR = '1') then
             AR <= R;
-            BW <= (others => 'Z');
-        else
-            AR <= (others => 'Z');
+            BW <= (others => '0');
+        elsif(Ww = '1')then
+            AR <= (others => '0');
             BW <= R;
+		else 
+			AR <= (others => '0');
+			BW <= (others => '0');
         end if;
     end process;
 End Behavioral;
+
 
 
